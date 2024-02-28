fc560a48c83c86721e2bca503214a41f7bde533f
Theo Technicguy <19630890+TheoTechnicguy@users.noreply.github.com>
2024-02-28 17:34:35 +0100

    feat(routes): create common middleware for CORS

---
+++ routes/middleware.go
@@ -0,0 +1,23 @@
+/**
+ * file: router/middleware.go
+ * author: theo technicguy
+ * license: apache-2.0
+ *
+ * This file contains common middleware.
+ */
+
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// CommonHeaders is a common middleware inserting common headers
+// that should be included in every response from the server.
+func CommonHeaders(ctx *gin.Context) {
+	ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	ctx.Writer.Header().Set("Access-Control-Max-Age", "300")
+	ctx.Writer.Header().Set("X-Content-Type-Options", "nosniff")
+	ctx.Next()
+}
