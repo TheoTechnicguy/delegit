87617bbeda2ad8e15cba3115708c7b9aa35048f5
Theo Technicguy <19630890+TheoTechnicguy@users.noreply.github.com>
2024-03-16 19:26:35 +0100

    feat(routes): add fonts path

--- routes/front.go
+++ routes/front.go
@@ -11,6 +11,7 @@ func RegisterFrontendRoutes(router *gin.Engine) {
 	router.Static("/styles", "public/styles")
 	router.Static("/js", "public/js")
 	router.Static("/images", "public/images")
+	router.Static("/fonts", "public/fonts")
 	router.LoadHTMLFiles("public/index.html")
 
 	router.GET("/", func(ctx *gin.Context) {
